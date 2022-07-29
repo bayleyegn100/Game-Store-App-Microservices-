94298e0a252fa226f900614df12ea420f53c8b64
Debasu Eyasu <bayleyegn100@gmail.com>
2022-07-29 01:19:36 -0700

    Creates a console feign client interface.

--- gamestore-invoicing/gamestore-invoicing/src/main/java/com/yedebkid/gamestoreinvoicing/util/feign/ConsoleMagicClient.java
+++ gamestore-invoicing/gamestore-invoicing/src/main/java/com/yedebkid/gamestoreinvoicing/util/feign/ConsoleMagicClient.java
@@ -1,2 +1,14 @@
-package com.yedebkid.gamestoreinvoicing.util.feign;public interface ConsoleMagicClient {
+package com.yedebkid.gamestoreinvoicing.util.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.io.Console;
+import java.util.List;
+
+@FeignClient(name= "console")
+public interface ConsoleMagicClient {
+    @RequestMapping(value = "/console", method = RequestMethod.GET)
+    public List<Console> getAllConsoles();
 }
