6d1c5bddc46aaa61d010997cd7c43a1dc9113aa3
Debasu Eyasu <bayleyegn100@gmail.com>
2022-07-28 23:30:14 -0700

    creates a tshirt repository interface that extends from JpaRepository.

--- gamestore-catalog/gamestore-catalog/src/main/java/com/yedebkid/gamestorecatalog/rpository/GameRepository.java
+++ gamestore-catalog/gamestore-catalog/src/main/java/com/yedebkid/gamestorecatalog/rpository/GameRepository.java
@@ -2,9 +2,11 @@ package com.yedebkid.gamestorecatalog.rpository;
 
 import com.yedebkid.gamestorecatalog.model.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
     List<Game> findAllByEsrbRating(String esrbRating);
     List<Game> findAllByStudio(String studio);
---
+++ gamestore-catalog/gamestore-catalog/src/main/java/com/yedebkid/gamestorecatalog/rpository/TShirtRepository.java
@@ -0,0 +1,12 @@
+package com.yedebkid.gamestorecatalog.rpository;
+
+import com.yedebkid.gamestorecatalog.model.TShirt;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface TShirtRepository extends JpaRepository<TShirt, Long> {
+    List<TShirt> findAllByColor(String color);
+    List<TShirt> findAllBySize(String size);
+}
