0a6283acd857c3d7ead5acea392380dbc5885dcc
Debasu Eyasu <bayleyegn100@gmail.com>
2022-07-29 01:25:10 -0700

    Adds console controller that uses the console feign client interface method.

--- gamestore-invoicing/gamestore-invoicing/src/main/java/com/yedebkid/gamestoreinvoicing/controller/InvoiceController.java
+++ gamestore-invoicing/gamestore-invoicing/src/main/java/com/yedebkid/gamestoreinvoicing/controller/InvoiceController.java
@@ -1,2 +1,31 @@
-package com.yedebkid.gamestoreinvoicing.controller;public class InvoiceController {
+package com.yedebkid.gamestoreinvoicing.controller;
+
+import com.yedebkid.gamestoreinvoicing.util.feign.ConsoleMagicClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.Console;
+import java.util.List;
+
+@RestController
+@RefreshScope
+public class InvoiceController {
+    @Autowired
+    private ConsoleMagicClient consoleMagicClient;
+
+    public InvoiceController(ConsoleMagicClient consoleMagicClient) {
+        this.consoleMagicClient = consoleMagicClient;
+    }
+
+    @GetMapping(value="/invoice")
+    public String getAllInvoice(){
+        return "To Do list";
+    }
+
+    @GetMapping(value= "/console")
+    public List<Console> getAllConsole(){
+        return consoleMagicClient.getAllConsoles();
+    }
 }
