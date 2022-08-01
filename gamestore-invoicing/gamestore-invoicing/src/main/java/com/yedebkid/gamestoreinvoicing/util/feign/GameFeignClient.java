b5ed63e2fe41e453cc60feff8349cab3fe7db985
Debasu Eyasu <bayleyegn100@gmail.com>
2022-07-31 19:06:02 -0700

    Adds console, game and tshirt feign client to make a request to registry to a desired destination.

--- gamestore-invoicing/gamestore-invoicing/src/main/java/com/yedebkid/gamestoreinvoicing/util/feign/GameFeignClient.java
+++ gamestore-invoicing/gamestore-invoicing/src/main/java/com/yedebkid/gamestoreinvoicing/util/feign/GameFeignClient.java
@@ -1,4 +1,26 @@
 package com.yedebkid.gamestoreinvoicing.util.feign;
 
+import com.yedebkid.gamestoreinvoicing.model.Game;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+import java.util.Optional;
+
+@FeignClient(name="gamestore-catalog")
 public interface GameFeignClient {
+    @RequestMapping(value="/game", method = RequestMethod.GET)
+    public List<Game> getAllGames();
+    @RequestMapping(value = "/game/{id}", method = RequestMethod.PUT)
+    public void updateGameById(@PathVariable long id, @RequestBody Game game);
+
+    @RequestMapping(value = "/game/{id}", method = RequestMethod.GET)
+    public Optional<Game> getGameById(@PathVariable long id);
+
+    @RequestMapping(value = "/game}", method = RequestMethod.POST)
+    public Game addGame(@RequestBody Game game);
 }
+
--- gamestore-invoicing/gamestore-invoicing/src/main/java/com/yedebkid/gamestoreinvoicing/util/feign/TShirtFeignClient.java
+++ gamestore-invoicing/gamestore-invoicing/src/main/java/com/yedebkid/gamestoreinvoicing/util/feign/TShirtFeignClient.java
@@ -1,5 +1,25 @@
 package com.yedebkid.gamestoreinvoicing.util.feign;
 
-public interface TShirtFeignClient {
+import com.yedebkid.gamestoreinvoicing.model.TShirt;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+import java.util.Optional;
+
+@FeignClient(name="gamestore-catalog")
+public interface TShirtFeignClient {
+    @RequestMapping(value="/tshirt", method = RequestMethod.GET)
+    public List<TShirt> getAllTShirt();
+    @RequestMapping(value = "/tshirt/{id}", method = RequestMethod.PUT)
+    public void updateTShirtById(@PathVariable long id, @RequestBody TShirt tshirt);
+
+    @RequestMapping(value = "/tshirt/{id}", method = RequestMethod.GET)
+    public Optional<TShirt> getTShirtById(@PathVariable long id);
+
+    @RequestMapping(value = "/tshirt}", method = RequestMethod.POST)
+    public TShirt addTShirt(@RequestBody TShirt tshirt);
 }
